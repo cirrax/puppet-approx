7b2ca88bddcf8a40ed294f81f1bb70235dd003e5
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2021-01-28 21:51:51 +0100

    update doc, use strings to generate REFERENCE

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,11 @@
 #
-# == Approx apt proxy server, main class
+# Approx apt proxy server, main class
 #
 # @summary Installs the approx server and a basic configuration file
 # specific repositories have to be defined with
 # apt::approx::repository.
 #
-# @examples hiera usage to configure repositories
-#
+# @example hiera usage to configure repositories
 #  approx::config:
 #    debian:
 #      url: 'http://ftp.ch.debian.org/debian'
@@ -15,26 +14,41 @@
 #    debian-backports:
 #      url: 'http://ftp.ch.debian.org/debian-backports'
 #
-# @param conffile path to the configuration file
-# @param cache path of the approx cache directory
-# @param ensure_cache if true it ensures that the cache directories are created
-# @param interval time  in  minutes  after which a cached file will be considered too old to deliver
-# @param max_rate maximum download rate from remote repositories, in bytes per second
-# @param max_redirects maximum number of HTTP redirections 
-# @param user user that owns the files in cache
-# @param group group that owns the files in cache
-# @param syslog syslog facility to use when logging
-# @param pdiffs whether to support IndexFile diffs
-# @param offline whether to deliver (possibly out-of-date) cached files
-# @param max_wait  how many seconds an approx(8) process will wait for a concurrent download
-# @param verbose whether informational messages should be printed in the log
-# @param debug whether debugging messages should be printed in the log
-# @param config Hash of repositories to configure 
-# @param create_resources  a Hash of Hashes to create additional resources
+# @param conffile 
+#   path to the configuration file
+# @param cache
+#   path of the approx cache directory
+# @param ensure_cache
+#   if true it ensures that the cache directories are created
+# @param interval
+#   time in minutes after which a cached file will be considered too old to deliver
+# @param max_rate
+#   maximum download rate from remote repositories, in bytes per second
+# @param max_redirects
+#   maximum number of HTTP redirections 
+# @param user
+#   user that owns the files in cache
+# @param group
+#   group that owns the files in cache
+# @param syslog
+#   syslog facility to use when logging
+# @param pdiffs
+#   whether to support IndexFile diffs
+# @param offline
+#   whether to deliver (possibly out-of-date) cached files
+# @param max_wait
+#   how many seconds an approx(8) process will wait for a concurrent download
+# @param verbose
+#   whether informational messages should be printed in the log
+# @param debug
+#   whether debugging messages should be printed in the log
+# @param config
+#   Hash of repositories to configure 
+# @param create_resources
+#   a Hash of Hashes to create additional resources
 #    Defaults to {} (do not create any additional resources)
 #
-#    Example (hiera) to create an xinetd service using puppetlabs-xinetd
-#
+# @example Example (hiera) to create an xinetd service using puppetlabs-xinetd
 #    approx::create_resources:
 #      xinetd::service:
 #        approx:
